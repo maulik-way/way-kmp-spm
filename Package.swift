9b53e0095fdc226972e70d6424c770d61b8190b2
Maulik <maulik.hirani@way.com>
2025-09-10 23:36:43 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Shared",
+    platforms: [
+        .iOS(.v16),
+    ],
+    products: [
+        .library(name: "Shared", targets: ["Shared"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Shared",
+            url: "https://github.com/Way-com/app-android/releases/download/KMP_0.0.1/Shared.xcframework.zip",
+            checksum:"eb3df434df8c76a7f7c97e48affaa322c2d10233c5d2eac4e455fafb7baa8170")
+    ]
+)
