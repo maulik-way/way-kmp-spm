0defd0c01f97d07db03811b1fe32c37b6137e911
Maulik <maulik.hirani@way.com>
2025-09-15 21:56:14 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/maulik-way/way-kmp-spm/releases/download/v0.0.1/Shared.xcframework.zip",
-            checksum:"eb3df434df8c76a7f7c97e48affaa322c2d10233c5d2eac4e455fafb7baa8170")
+            url: "https://github.com/maulik-way/way-kmp-spm/releases/download/v0.0.2/Shared.xcframework.zip",
+            checksum:"69c2c6dba1973bff371936b78d6f9d29fe5c299f8b35ea24f6f24c4fd74d7219")
     ]
 )
